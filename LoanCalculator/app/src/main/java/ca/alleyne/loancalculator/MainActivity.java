aeb9b89989e6d9f98f572068ceb9119506d12d32
Keylen Alleyne <Keeson@hotmail.com>
2017-09-07 16:10:39 -0400

    Begin working on logic

--- LoanCalculator/app/src/main/java/ca/alleyne/loancalculator/MainActivity.java
+++ LoanCalculator/app/src/main/java/ca/alleyne/loancalculator/MainActivity.java
@@ -2,12 +2,40 @@ package ca.alleyne.loancalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    int numberOfYears;
+    double loanAmount, yearlyInterestRate;
+    TextView monthResult,totalResult,interestResult;
+    EditText loanInsert,termLoanHint, yearlyRateHint;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loanInsert = (EditText) findViewById(R.id.loanInsert);
+        termLoanHint = (EditText) findViewById(R.id.termLoanInsert);
+        yearlyRateHint = (EditText) findViewById(R.id.yearInterestInsert);
+        monthResult = (TextView) findViewById(R.id.monthResult);
+        totalResult = (TextView) findViewById(R.id.totalResult);
+        interestResult = (TextView) findViewById(R.id.interestResult);
+    }//onCreate
+
+    public void monthlyPayment(View v){
+
+    }
+
+    public void totalPayment(View v){
+
+    }
+
+    public void totalInterest(View v){
+
     }
 }
